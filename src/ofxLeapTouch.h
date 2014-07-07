3afa6b85d1bc35e9f2d20af449d608de2948a635
Felix Lange <langefx@gmail.com>
2014-07-07 12:36:39 +0200

    comments

--- src/ofxLeapTouch.h
+++ src/ofxLeapTouch.h
@@ -27,11 +27,19 @@ public:
 	virtual ~ofxLeapTouch();
 
 	void setup();
+
+	/* process leap motion data hand send touch events
+
+	Optional parameter to turn off marking current frame as old after processing,
+	so that it is possible to do additional stuff with the sensor.
+	Important: Application then has to call markFrameAsOld() by itself! */
 	bool update(bool markFrameAsOld = true);
+
+	//"debug" draw
 	void drawFingers();
 	void drawHands();
 
-	ofxLeapMotion & getLeapMotionObj(){return leap;}
+	ofxLeapMotion & getLeapMotionRef(){return leap;}
 
 #ifdef USE_OFX_GUI
 	ofxPanel gui;
@@ -54,6 +62,8 @@ public:
 
 protected:
 	ofPoint getScreenCoord(ofPoint & finger);
+
+	//process hand and finger information and possibly send touch events
 	void touchlessToTouch(touchlessTouchPoint & p, int id, float grabStrength = 0);
 
 	map <int, ofPolyline> fingerTrails;
